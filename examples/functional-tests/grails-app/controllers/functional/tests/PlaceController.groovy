2e081ec24bb2e165b9b65a0d108f07078a2badac
James Daugherty <jdaugherty@jdresources.net>
2024-12-21 10:23:13 -0500

    Remove mongo dependency & move json-templates to mongo repo

--- examples/functional-tests/grails-app/controllers/functional/tests/PlaceController.groovy
+++ examples/functional-tests/grails-app/controllers/functional/tests/PlaceController.groovy
@@ -1,7 +1,5 @@
 package functional.tests
 
-import grails.mongodb.geo.Point
-
 /**
  * Created by graemerocher on 19/05/16.
  */
@@ -9,16 +7,12 @@ class PlaceController {
 
     static responseFormats = ['json']
 
-    def show() {
-        respond new Place(name: "London", location: Point.valueOf(10,10))
-    }
-
     def test() {
-        respond new Place(name: "London", location: Point.valueOf(10,10))
+        respond new Place(name: "London", location: "UK")
     }
 }
 
 class Place {
     String name
-    Point location
+    String location
 }
